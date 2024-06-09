e4f91f22f16106b8e5c4e33add643e0f25fed6b2
Daiki Nohara <srotorqt@gmail.com>
2024-06-09 21:46:05 +0900

    add: エラーコードの追加

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,8 +12,17 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 
-		String password = "password123";
-		System.out.println("Password: " + password);
+		try {
+			File file = new File("file.txt");
+			Scanner scanner = new Scanner(file);
+
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found.");
+		}
 	}
 
 }
