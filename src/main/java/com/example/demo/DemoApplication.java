090c4ab251d645fc72a9329aeecec7468e27c2c0
Daiki Nohara <srotorqt@gmail.com>
2024-06-12 00:53:07 +0900

    fix: エラーの修正

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,5 @@
 package com.example.demo;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,32 +8,5 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
-		try {
-			File file = new File("file.txt");
-			Scanner scanner = new Scanner(file);
-
-			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				System.out.println(line);
-			}
-
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-		}
-
-		try {
-			File file = new File("file.txt");
-			Scanner scanner = new Scanner(file);
-
-			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				System.out.println(line);
-			}
-
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-		}
 	}
-
 }
